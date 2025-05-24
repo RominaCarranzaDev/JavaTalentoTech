24a04101aa8640746bcad34c8f857ad5afff6977
Romina Carranza <romii.carranza@gmail.com>
2025-05-23 22:31:47 -0300

    create pedido

---
+++ src/main/java/techlab/proyectoJava/Pedido.java
@@ -0,0 +1,37 @@
+package techlab.proyectoJava;
+
+public class Pedido {
+    private final Product producto;
+    private int cantidad;
+
+    public Pedido(Product producto, int cantidad){
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getMontoTotal() {
+        return this.cantidad * this.producto.getPrice();
+    }
+
+    public double getMontoUn() {
+        return this.producto.getPrice();
+    }
+
+    public void info() {
+        System.out.printf("""
+            Producto: %s
+            --------------------------------
+            Cantidad: %d
+            --------------------------------
+            Precio Unitario: % .2f
+            --------------------------------
+            Total: $ %s
+            ================================
+            """, this.producto.getName(), this.cantidad, getMontoUn(), getMontoTotal());
+
+    }
+}
